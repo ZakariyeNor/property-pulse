95160db2858dffef7c693edd90a65d67850aa01f
Zakariye <zakilenor@gmail.com>
2025-09-03 16:55:58 +0200

    Fetch data from Compass Api

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,24 +1,39 @@
-import React from 'react'
-import properties from '@/properties.json';
-import PropertyCard from '@/components/PropertyCard';
+import React from "react";
+import PropertyCard from "@/components/PropertyCard";
 
-const PropertiesPage = () => {
+async function fetchProperties() {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const PropertiesPage = async () => {
+  const properties = await fetchProperties();
+
+  properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
   return (
     <section class="px-4 py-6">
       <div class="container-xl lg:container m-auto px-4 py-6">
-       { properties.length === 0 ? (
-        'No properties found'
+        {properties.length === 0 ? (
+          "No properties found"
         ) : (
-           <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-          { properties.map(property => (
-            <PropertyCard key={property._id} property={property} />
-          )) }
-        </div>
-        ) }
+          <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
       </div>
     </section>
-          
-  )
-}
+  );
+};
 
-export default PropertiesPage
+export default PropertiesPage;
